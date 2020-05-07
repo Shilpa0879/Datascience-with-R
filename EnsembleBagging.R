88e7f8f56a87b69b61c0a2b839a7b83080217c1d
Shilpa0879 <63416694+Shilpa0879@users.noreply.github.com>
2020-05-07 19:40:51 +0530

    Add files via upload

--- EnsembleBagging.R
+++ EnsembleBagging.R
@@ -1,7 +1,7 @@
 data('iris')
 install.packages("ipred")
 library(ipred)
-
+##Bagging
 set.seed(300)
 
 mybag<-bagging(iris$Species~.,data = iris,nbagg = 50)
@@ -25,3 +25,24 @@ mybag
 credit_pred<-predict(mybag,train[,-5])
 credit_pred
 table(credit_pred,train$Species)
+
+##Boosting
+#Data partition for model building and testing
+library(caret)
+inTraininglocal<-createDataPartition(iris$Species,p=.70,list=F)
+training<-iris[inTraininglocal,]
+testing<-iris[-inTraininglocal,]
+
+#Model Building
+library(C50)
+model<-C5.0(training$Species~.,data=training,trials=20)  #Trails Boosting parameter
+model
+#Generate the model sumamry
+summary(model)
+#Predict for test data set
+pred<-predict.C5.0(model,testing[,-5])
+pred
+a<-table(testing$Species,pred)
+a
+sum(diag(a))/sum(a)
+
